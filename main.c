753fc617540cc7dc1cbea7f240d5ea7915bd5d05
J08nY <johny@neuromancer.sk>
2023-05-14 19:57:41 +0200

    Add keygen, export and signing.

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include "uecc/uECC.h"
 #include <stddef.h>
 
-uint32_t state = 0xdeadbeef;
+static uint32_t state = 0xdeadbeef;
 
-uint32_t xorshift32(void) {
+static uint32_t xorshift32(void) {
     state ^= state << 13;
     state ^= state >> 17;
     state ^= state << 5;
@@ -13,8 +13,11 @@ uint32_t xorshift32(void) {
 }
 
 static uint8_t cmd_init_prng(uint8_t *data, uint16_t len) {
-    state = ((uint32_t) data[0]) << 24 || ((uint32_t) data[1]) << 16 || ((uint32_t) data[2]) << 8 || ((uint32_t) data[3]);
-    return 0;
+    state = ((uint32_t) data[0]) << 24 ||
+            ((uint32_t) data[1]) << 16 ||
+            ((uint32_t) data[2]) << 8 ||
+            ((uint32_t) data[3]);
+    return 1;
 }
 
 int uECC_prng(uint8_t *dest, unsigned size) {
@@ -23,26 +26,46 @@ int uECC_prng(uint8_t *dest, unsigned size) {
         for (unsigned j = 0; j < 4; j++) {
             unsigned idx = i * 4 + j;
             if (idx < size) {
-                dest[idx] = (uint8_t)((rand >> 8 * j) & 0xff);
+                dest[idx] = (uint8_t) ((rand >> 8 * j) & 0xff);
             }
         }
     }
     return 1;
 }
 
+static uint8_t pubkey[64];
+static uint8_t privkey[32];
+
+static uint8_t cmd_generate_keypair(uint8_t *data, uint16_t len) {
+    return uECC_make_key(pubkey, privkey, uECC_secp256r1());
+}
+
+static uint8_t cmd_export(uint8_t *data, uint16_t len) {
+    simpleserial_put('w', 64, pubkey);
+    return 1;
+}
+
+static uint8_t cmd_sign(uint8_t *data, uint16_t len) {
+    uint8_t signature[64];
+    int res = uECC_sign(privkey, data, len, signature, uECC_secp256r1());
+
+    simpleserial_put('s', 64, signature);
+    return res;
+}
+
 int main(void) {
     uECC_set_rng(&uECC_prng);
-
-    //uECC_Curve secp256r1 = uECC_secp256r1();
-	//uECC_shared_secret(NULL, NULL, NULL, secp256r1);
 
     platform_init();
     init_uart();
     trigger_setup();
     simpleserial_init();
-    simpleserial_addcmd('i', MAX_SS_LEN, cmd_init_prng);
+    simpleserial_addcmd('i', 4, cmd_init_prng);
+    simpleserial_addcmd('g', 0, cmd_generate_keypair);
+    simpleserial_addcmd('e', 0, cmd_export);
+    simpleserial_addcmd('s', MAX_SS_LEN, cmd_sign);
 
-    while(simpleserial_get());
+    while (simpleserial_get());
 
     return 0;
 }
